ee0a4e0cd10e23d5210c058e272172a52623793a
Sergey Pavlov <zaqo@narod.ru>
2018-05-26 17:42:07 +0300

    Loop for file parsing file
    transactions log

--- loader_actions.php
+++ loader_actions.php
@@ -18,48 +18,51 @@ include ("header.php");
 	{
 			$in='';
 			$in= fgets($fp);
-			echo $in;
+			//echo $in;
 			//$in_=iconv('windows-1251','utf-8',$in);
 			$in__=explode(";",$in);
 			//foreach($in__ as $value)
 			//	echo $value.'<br/>';
-			$in= fgets($fp);
-			$in_=iconv('windows-1251','utf-8',$in);
-			$in__=explode(";",$in_);
-			$time_st=$in__[1]." ".$in__[3];
-			echo $time_st;
-			$usr_sap=$in__[4];
-			$terminal=$in__[5];
-			$trn=$in__[6];
-			$prog=$in__[7];
-			$about=$in__[8];
+			while(!feof($fp))
+			{
+				$in= fgets($fp);
+				$in_=iconv('windows-1251','utf-8',$in);
+				$in__=explode(";",$in_);
+				$time_st=$in__[1]." ".$in__[3];
+			
+				$usr_sap=$in__[4];
+				$terminal=$in__[5];
+				$trn=$in__[6];
+				$prog=$in__[7];
+				$about=$in__[8];
 			//echo $time_st.'<br/>';
-			$get_that_user='SELECT user_id FROM sap WHERE sap_id="'.$usr_sap.'"';
-			$answsqlnext=mysqli_query($db_server,$get_that_user);
-			if(!$answsqlnext) die("SELECT FROM user TABLE failed: ".mysqli_error($db_server));
-			$row = mysqli_fetch_row($answsqlnext);
-			if($row)	$user=$row[0];
-			else echo 'NO USER RECORD FOUND <br/>';
+				$get_that_user='SELECT user_id FROM sap WHERE sap_id="'.$usr_sap.'"';
+				$answsqlnext=mysqli_query($db_server,$get_that_user);
+				if(!$answsqlnext) die("SELECT FROM user TABLE failed: ".mysqli_error($db_server));
+				$row = mysqli_fetch_row($answsqlnext);
+				if($row)	$user=$row[0];
+					else echo 'NO USER RECORD FOUND <br/>';
 	
-			$get_that_trn='SELECT id FROM transactions WHERE code="'.$trn.'"';
-			$answsqlnext=mysqli_query($db_server,$get_that_trn);
-			if(!$answsqlnext) die("SELECT FROM user TABLE failed: ".mysqli_error($db_server));
-			$row = mysqli_fetch_row($answsqlnext);
-			if($row)	$trn=$row[0];
-			else echo 'NO TRANSACTION RECORD FOUND <br/>';
+				$get_that_trn='SELECT id FROM transactions WHERE code="'.$trn.'"';
+				$answsqlnext=mysqli_query($db_server,$get_that_trn);
+				if(!$answsqlnext) die("SELECT FROM user TABLE failed: ".mysqli_error($db_server));
+				$row = mysqli_fetch_row($answsqlnext);
+				if($row)	$trn=$row[0];
+				else echo 'NO TRANSACTION RECORD FOUND <br/>';
 	
-			$dtime = DateTime::createFromFormat("d.m.Y H:i:s", $time_st);
-			$timestamp = $dtime->getTimestamp();
-			$transfer_mysql='INSERT INTO activity_reg
+				$dtime = DateTime::createFromFormat("d.m.Y H:i:s", $time_st);
+				$timestamp = $dtime->getTimestamp();
+				$transfer_mysql='INSERT INTO activity_reg
 					(user_id,trn_id,date,prog,message,terminal) 
 					VALUES
 					("'.$user.'","'.$trn.'",FROM_UNIXTIME('.$timestamp.'),"'.$prog.'","'.$about.'","'.$terminal.'")';
 								
-			$answsql=mysqli_query($db_server,$transfer_mysql);
+				$answsql=mysqli_query($db_server,$transfer_mysql);
 							
-			if(!$answsql) die("INSERT into TABLE failed: ".mysqli_error($db_server));
-	}
-	else echo "NOT FOR END USERS. TESTING ONLY!!!";
+				if(!$answsql) die("INSERT into TABLE failed: ".mysqli_error($db_server));
+			}
+		}
+		else echo "NOT FOR END USERS. TESTING ONLY!!!";
 	//var_dump($timestamp);
 	/*
 	while(!feof($fp)) {
